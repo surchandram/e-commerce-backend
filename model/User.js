073bb91f544f77be2fca504d59286c91c1987807
surchandram <surrrchandra@gmail.com>
2024-02-21 12:22:53 +0530

    Adding User model

---
+++ model/User.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const UserSchem = new Schema({
+    fullname:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    orders:[]
+})
