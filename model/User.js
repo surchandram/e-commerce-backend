e4cd263d36fca7d8de34dc5802d150b32f4ee658
surchandram <surrrchandra@gmail.com>
2024-02-21 21:20:55 +0530

    mongoose connection

--- model/User.js
+++ model/User.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 
-const UserSchem = new Schema({
+const UserSchema= new Schema({
     fullname:{
         type: String,
         required: true,
@@ -14,5 +14,61 @@ const UserSchem = new Schema({
         type: String,
         required: true,
     },
-    orders:[]
-})
+    orders:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+        }
+       ], 
+    wishLists:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "WishList",
+        }
+       ], 
+    isAdmin: {
+        type: Boolean,
+        default: false,
+       },
+    hasShippingAddress: {
+        type: Boolean,
+        default: false,
+       },
+    ShippingAddress: {
+        firstName: {
+            type: String,
+        },
+        lastName: {
+            type: String,
+        },
+        address: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        postalCode: {
+            type: String,
+        },
+        state: {
+            type: String,
+        },
+        country: {
+            type: String,
+        },
+        phone: {
+            type: String,
+        },
+       }
+},
+{
+    timestamps: true,
+}
+
+);
+
+//compile the schema to model
+
+const User = mongoose.model("user", UserSchema);
+
+export default User;
