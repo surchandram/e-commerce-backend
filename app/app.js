5f1c0b2ef08fea93bf0c30112db668c18eac9195
MOIRANGTHEM SURCHANDRA SINGH <65487222+surchandram@users.noreply.github.com>
2024-04-22 16:25:54 +0530

    add controller

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express  from "express";
 import dbConnect from "../config/dbConnect.js";
 
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 const dbConnect = async ()=>{
     try{
         mongoose.set('strictQuery',false);
-        const connected = mongoose.connect('mongodb+srv://sur:FMb2UlTPsA2uBxyp@nodejs-ecommerce-api.3thbrki.mongodb.net/e-commerce-api?retryWrites=true&w=majority&appName=nodejs-ecommerce-api');
-        console.log(`Mongodb connected ${(await connected).connection.host}`);
+        const connected =await mongoose.connect(process.env.MONGO_URL);
+        console.log(`Mongodb connected ${connected.connection.host}`);
     } catch(error){
         console.log(`Error: ${error.message}`);
         process.exit(1);
